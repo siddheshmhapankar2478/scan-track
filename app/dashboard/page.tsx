00638e34db674c596b9f8e838e567c385aa56016
siddheshmhapankar <siddhesh.m@logiqids.com>
2025-11-09 13:36:12 +0530

    header section added

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+const Dashboard = () => {
+  return (
+    <div className="mt-24">
+      <p>Dashboard</p>
+    </div>
+  );
+};
+export default Dashboard;
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,22 +5,57 @@ import {
   SignUpButton,
   UserButton,
 } from "@clerk/nextjs";
+import Image from "next/image";
 import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { LayoutDashboard, PenBox } from "lucide-react";
 
 const Header = () => {
+  const links = [
+    {
+      name: "Dashboard",
+      icon: <LayoutDashboard size={18} />,
+      href: "/dashboard",
+      variant: "outline",
+    },
+    {
+      name: "Transactions",
+      icon: <PenBox size={18} />,
+      href: "/transactions",
+      variant: "default",
+    },
+  ];
   return (
-    <div className="fixed top-0 w-full bg-blue-200 text-gray-600 p-12 flex items-center justify-center">
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton>
-          <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-            Sign Up
-          </button>
-        </SignUpButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+    <div className="fixed top-0 w-full bg-[#1C4259] text-gray-600 px-12 py-2 flex items-center justify-between shadow-lg z-50">
+      <Link href={"/"}>
+        <Image width={200} height={150} src="/scan-track-logo.png" alt="logo" />
+      </Link>
+
+      <div className="flex gap-4">
+        <SignedOut>
+          <SignInButton forceRedirectUrl={"/dashboard"}>
+            <Button variant="outline">Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+
+        <SignedIn>
+          {links.map((link, index) => (
+            <Link
+              key={index}
+              href={link.href}
+              className="text-gray-600 hover:text-blue-600 flex gap-2 items-center"
+            >
+              <Button variant={link.variant}>
+                {link.icon}
+                <span className="hidden md:inline">{link.name}</span>
+              </Button>
+            </Link>
+          ))}
+
+          <UserButton appearance={{ elements: { avatarBox: "w-10 h-10" } }} />
+        </SignedIn>
+      </div>
     </div>
   );
 };
